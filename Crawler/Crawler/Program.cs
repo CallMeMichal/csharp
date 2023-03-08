7bc60c1748f415937c0219a98a2c7ec7b786ff52
CallMeMichal <michaltulej1@gmail.com>
2023-03-08 15:27:23 +0100

    HttpResponseMessage

--- Crawler/Crawler/Program.cs
+++ Crawler/Crawler/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length == 0)
+            {
+                throw new ArgumentNullException();
+            }
+
+            string url = args[0];
+
+            // zadanie http to klasa wbudowane
+            // ctr+kc comment
+            // ctr+ku uncomment
+
+
+            //stara wersja bez using
+            HttpClient client = new();
+            client.Dispose(); //zwalnianie miejsca
+
+            //zrobienie scopa automatycznie uzywa sie dispose , zwoli sie automatycznie poprzez uzycie using
+            using HttpClient httpClient = new();
+
+            //asynchroniczna metoda do zwracania czegos, zwracaja taska . await odpakowywuje dane ktore wracają . jak chcemy uzywac metod asynchronicznych to dodajemy async do maina obok void
+            // w asynchronicznosci nie ma blokowania głownego wątku
+            // poprzez await rozpakowywuje sie i dotaje do srodka zapytania 
+            HttpResponseMessage result = await httpClient.GetAsync(url);
+
+           
+
+
         }
     }
 }
