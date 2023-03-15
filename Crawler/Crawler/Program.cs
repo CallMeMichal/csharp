2a29a32100f65230e406c8dc4169dc819e488117
CallMeMichal <michaltulej1@gmail.com>
2023-03-15 14:38:54 +0100

    -

--- Crawler/Crawler/Program.cs
+++ Crawler/Crawler/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace Crawler
 {
@@ -6,19 +7,22 @@ namespace Crawler
     {
         static async void Main(string[] args)
         {
+            //1wymaganie
             if (args.Length == 0)
             {
                 throw new ArgumentNullException();
             }
 
             string url = args[0];
-            
+            //2 wymaganie
             url pattern = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)\r\n";
             Regex urlRegex = new(pattern);
             if (!urlRegex.IsMatch(url))
             {
                 throw new ArgumentException();
             }
+
+
 
             //stara wersja bez using
             HttpClient client = new();
@@ -31,9 +35,23 @@ namespace Crawler
             // w asynchronicznosci nie ma blokowania głownego wątku
             // poprzez await rozpakowywuje sie i dotaje do srodka zapytania 
             HttpResponseMessage result = await httpClient.GetAsync(url);
+            //3 wymaganie
+            if (!result.IsSuccessStatusCode)
+            {
+                throw new Exception("bląd podczas pobierania strony")
+            }
+            
             string content = await result.Content.ReadAsStringAsync();
 
-           
+            //await rozpakowywuje strina i odstaje sie do pliku
+            string htmlcontent = await result.Content.ReadAsStringAsync();
+            // ignor case zabezpiecza przed duza litera jak ktos sie pomyli
+            string emailPattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+            Regex emailRegex new(emailPattern,RegexOptions.IgnoreCase);
+
+            //zwraca emaile ktore spelnily oczekiwania
+            MatchCollection matchedEmails = emailRegex.Matches(htmlcontent)
+
 
 
         }
@@ -44,3 +62,6 @@ namespace Crawler
 // zadanie http to klasa wbudowane
 // ctr+kc comment
 // ctr+ku uncomment
+//kluczyki properties szesciany metody
+
+//https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
